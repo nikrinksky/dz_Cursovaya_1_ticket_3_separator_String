df73ae03ef4a20712071928b4d3d9a9c3a5e68fb
Nik <nik_rink@ukr.net>
2023-07-17 00:06:01 +0300

    Сделал решение задачи билета № 3(разделение строки) в ветке ticket3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+
+//Задача билета №3
+//Напишите программу, которая разделяет любую строку на две части ровно пополам
+// по количеству символов.
 public class Main {
+    public static void separator (String str) {
+        int middle = str.length() / 2;
+        String first = str.substring(0, middle);
+        String second = str.substring(middle);
+        System.out.println(first);
+        System.out.println(second);
+    }
 
     public static void main(String[] args) {
+        separator("Тестовая строка");
+        System.out.println();
+        separator("Тестовая строка2");
 
-        System.out.println("Hello world!");
     }
 }
